405d95f5786a38dbf3b82b28f98cc9d9be572c53
Hutr0 <Hutro-games@yandex.ru>
2021-02-17 04:07:30 +0300

    MainView to MVVM

---
+++ ToDoList/ViewModel/MainViewModel.swift
@@ -0,0 +1,75 @@
+//
+//  MainViewModel.swift
+//  ToDoList
+//
+//  Created by Леонид Лукашевич on 17.02.2021.
+//
+
+import UIKit
+import CoreData
+
+class MainViewModel {
+    
+    var context: NSManagedObjectContext!
+    
+    var tasks: [Task] = []
+    
+    func loadData() {
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        do {
+            try tasks = context.fetch(fetchRequest)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func numberOfRowsInSection() -> Int {
+        return tasks.count
+    }
+    
+    func configureTheCell(_ tableView: UITableView, for indexPath: IndexPath) -> Cell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! Cell
+        
+        let task = tasks[indexPath.row]
+        
+        cell.nameLabel.text = task.title
+        cell.descriptionLabel.text = task.desc
+        if let pictureOfTask = task.picture {
+            cell.picture.image = UIImage(data: pictureOfTask)
+        } else {
+            cell.picture.image = #imageLiteral(resourceName: "nothing")
+        }
+        
+        return cell
+    }
+    
+    func removeObject(at indexPath: IndexPath) {
+        let task = tasks[indexPath.row]
+        StorageManager.deleteObject(context: context, object: task)
+        tasks.remove(at: indexPath.row)
+    }
+    
+    func prepareEditTask(for segue: UIStoryboardSegue, at indexPath: IndexPath) {
+        let vc = segue.destination as! AddViewController
+        let task = tasks[indexPath.row]
+        
+        vc.currentContext = task.managedObjectContext
+        vc.task = task
+    }
+    
+    func unwindSegue(_ segue: UIStoryboardSegue, closure: @escaping () -> ()) {
+        guard let source = segue.source as? AddViewController else { return }
+        
+        source.currentContext = context
+        source.saveTask()
+        
+        guard let task = source.task else {
+            closure()
+            return
+        }
+        
+        tasks.append(task)
+        closure()
+    }
+}
